e5acedaac2fdad1d19963278c7da7a79833319ec
ZeynalovaZahra <zahrazeynalova011@gmail.com>
2023-12-11 10:37:57 +0400

    JS2

--- js/app.js
+++ js/app.js
@@ -40,115 +40,169 @@
 // if (i===0){
 
 // }
-var names = [
-    {
-        name: "Murad",
-        gender: "male",
-    },
-    {
-        name: "Eliyar",
-        gender: "male",
-    },
-    {
-        name: "Harun",
-        gender: "male",
-    },
-    {
-        name: "Celil",
-        gender: "male"
-    },
-    {
-        name: "Yusif",
-        gender: "male"
-    },
-    {
-        name: "Ulvi",
-        gender: "male"
-    },
-    {
-        name: "Raul",
-        gender: "male"
-    },
-    {
-        name: "Ruslan",
-        gender: "male"
-    },
-    {
-        name: "Ehmed",
-        gender: "male"
-    },
-    {
-        name: "Ayla",
-        gender: "female"
-    },
-    {
-        name: "Inci",
-        gender: "female",
-    },
-    {
-        name: "Nigar",
-        gender: "female",
-    },
-    {
-        name: "Ulviyye",
-        gender: "female",
-    },
-    {
-        name: "Naile",
-        gender: "female"
-    },
-    {
-        name: "Emiliya",
-        gender: "female"
-    },
-    {
-        name: "Zehra",
-        gender: "female",
-    },
-    {
-        name: "Zeyneb",
-        gender: "female",
-    },
-    {
-        name: "Aydan",
-        gender: "female",
-    },
-    {
-        name: "Sema",
-        gender: "female",
-    },
-];
-var surnames = [
-    "Zeynalova",
-    "Ceferli",
-    "Hesenov",
-    "Memmedova",
-    "Axundova",
-    "Muradov",
-    "Ebilova",
-    "Xelilov",
-    "Dadasov",
-    "Yusifov",
-    "Balayev",
-    "Abuyeva",
-    "Muradov",
-    "Efendiyev",
-    "Allahverdiyev",
-    "Zeynalov",
-    "Haqverdiyev",
-    "Gulmahammadov",
-    "Agayeva",
-];
+// var names = [
+//     {
+//         name: "Murad",
+//         gender: "male",
+//     },
+//     {
+//         name: "Eliyar",
+//         gender: "male",
+//     },
+//     {
+//         name: "Harun",
+//         gender: "male",
+//     },
+//     {
+//         name: "Celil",
+//         gender: "male"
+//     },
+//     {
+//         name: "Yusif",
+//         gender: "male"
+//     },
+//     {
+//         name: "Ulvi",
+//         gender: "male"
+//     },
+//     {
+//         name: "Raul",
+//         gender: "male"
+//     },
+//     {
+//         name: "Ruslan",
+//         gender: "male"
+//     },
+//     {
+//         name: "Ehmed",
+//         gender: "male"
+//     },
+//     {
+//         name: "Ayla",
+//         gender: "female"
+//     },
+//     {
+//         name: "Inci",
+//         gender: "female",
+//     },
+//     {
+//         name: "Nigar",
+//         gender: "female",
+//     },
+//     {
+//         name: "Ulviyye",
+//         gender: "female",
+//     },
+//     {
+//         name: "Naile",
+//         gender: "female"
+//     },
+//     {
+//         name: "Emiliya",
+//         gender: "female"
+//     },
+//     {
+//         name: "Zehra",
+//         gender: "female",
+//     },
+//     {
+//         name: "Zeyneb",
+//         gender: "female",
+//     },
+//     {
+//         name: "Aydan",
+//         gender: "female",
+//     },
+//     {
+//         name: "Sema",
+//         gender: "female",
+//     },
+// ];
+// var surnames = [
+//     "Zeynalov",
+//     "Ceferli",
+//     "Hesenov",
+//     "Memmedov",
+//     "Axundov",
+//     "Muradov",
+//     "Ebilov",
+//     "Xelilov",
+//     "Dadasov",
+//     "Yusifov",
+//     "Balayev",
+//     "Abuyev",
+//     "Muradov",
+//     "Efendiyev",
+//     "Allahverdiyev",
+//     "Zeynalov",
+//     "Haqverdiyev",
+//     "Gulmahammadov",
+//     "Agayev",
+// ];
 
-var randomNameIndex = Math.floor(Math.random() * 19);
-var randomName = names[randomNameIndex].name;
-var randomSurnameIndex = Math.floor(Math.random() * 19);
-var randomSurname = surnames[randomSurnameIndex];
-if (names[randomNameIndex].gender === "male") {
-    console.log(randomSurname + " " + randomName);
-} else {
-    console.log(randomSurname + "a" + " " + randomName);
-}
+// var randomNameIndex = Math.floor(Math.random() * 19);
+// var randomName = names[randomNameIndex].name;
+// var randomSurnameIndex = Math.floor(Math.random() * 19);
+// var randomSurname = surnames[randomSurnameIndex];
+// if (names[randomNameIndex].gender === "male") {
+//     console.log(randomSurname + " " + randomName);
+// } else {
+//     console.log(randomSurname + "a" + " " + randomName);
+// }
+
+// var alphabet = ("a", "b", "c", "d", "e", "o", "p" ,"r","s");
+// var name = '';
+// for (let i = 0; i < length; i++);{
+//     var randomIndex = MathFloor(Math.random() *alphabet.length);
+//    console.log( name +alphabet)
+// }
+//  Task 1;
+
+// const birthDate = new Date('2006-09-26');
+// const age = calculateAge(birthDate);
+// console.log(age); ????
+// duzu burda:
+// let age = prompt("dogum ilini qeyd ele !");
+// console.log(2023 - age);
+
+//  Task 2;
+
+//     let password = "abvsnhchbhdhdkhch";
+//     for (let i = 0; i < length; i++) {
+//       const randomIndex = Math.floor(Math.random() * charAt.length);
+//       password += charAt[randomIndex];
+//     }
+//   console.log(password);
+
+// Task 3;
+//  var sentence = "Daxili Isler Nazirliyi";
+//  var words = sentence.split(" ");
+//  var secondWord = words.length >= 2 ? words[1] : null;
+//  console.log("secondString", secondString);
+
+// Task 4;
+//   var name ="Memmedova";
+
+
+// Task 6;
+// var sentence = prompt("Cumle yaz");
+// var bigword = sentence.charAt(0).toUpperCase() + sentence.substr(1);
+// console.log("", bigword);
+
+
+
+
+
+// Task 7;
+var sentence = prompt("Cumle yaz");
+var smallword = sentence.substr(0, -1) + sentence.substr(-1).toUpperCase();
+console.log("", smallword);
+
+
+
+
+
+
 
 
 
